bb1ea4a3ca4882926d110a005ac56fa685479746
alejo <olavealejito@gmail.com>
2022-11-18 11:00:39 -0500

    modificando menu

---
+++ js/media-query.js
@@ -0,0 +1,10 @@
+let barMenu = Document.getElementById("barmenu");
+let mainMenu = Document.getElementById("mainmenu");
+
+barMenu.addEventListener("click",function(){
+    if mainMenu ,removeEventlistener.classlist.contains("main-menu")){
+
+    }
+    mainMenu.classlist.add("main-menu-visible");
+    mainMenu.classlist.remove("main-menu");
+});
