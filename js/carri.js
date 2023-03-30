0fc47dced7c4e44eea8bbf0d72cbd9186fe8a034
Sharit-vargas07 <sharitvargas07@gmail.com>
2023-03-30 10:48:16 -0500

    mmmm

--- js/carri.js
+++ js/carri.js
@@ -1,5 +1,21 @@
 let imgbox = document.getElementById("imgbox");
-let imgmini1 = document.getElementById("imgmini1");
-let imgmini2 = document.getElementById("imgmini2");
-let imgmini3 = document.getElementById("imgmini3");
-let imgmini4 = document.getElementById("imgmini4");
+let imgMini1 = document.getElementById("imgMini1");
+let imgMini2 = document.getElementById("imgMini2");
+let imgMini3 = document.getElementById("imgMini3");
+let imgMini4 = document.getElementById("imgMini4");
+
+imgMini1.addEventListener("click",function(){
+    imgbox.setAttribute("src","img/moto-1.jpg");
+});
+
+imgMini2.addEventListener("click",function(){
+    imgbox.setAttribute("src","img/moto-2.png");
+});
+
+imgMini3.addEventListener("click",function(){
+    imgbox.setAttribute("src","img/moto-3.jpg");
+});
+
+imgMini4.addEventListener("click",function(){
+    imgbox.setAttribute("src","img/moto-4.png");
+});
