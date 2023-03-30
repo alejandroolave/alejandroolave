af2a48e8b3345ed70a96adc855b94a2a3f99a398
Sharit-vargas07 <sharitvargas07@gmail.com>
2023-03-30 10:00:12 -0500

    mmmm

---
+++ js/carri.js
@@ -0,0 +1,5 @@
+let imgbox = document.getElementById("imgbox");
+let imgmini1 = document.getElementById("imgmini1");
+let imgmini2 = document.getElementById("imgmini2");
+let imgmini3 = document.getElementById("imgmini3");
+let imgmini4 = document.getElementById("imgmini4");
