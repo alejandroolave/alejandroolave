af330c9d6badfd414f029614d3cd3c83c383a6de
YipGiH <dajozavargas@gmail.com>
2023-04-13 10:55:25 -0500

    add fuentes

--- js/carri.js
+++ js/carri.js
@@ -19,3 +19,30 @@ imgMini3.addEventListener("click",function(){
 imgMini4.addEventListener("click",function(){
     imgbox.setAttribute("src","img/pulsarmania.jpg");
 });
+let boton1 = document.getElementById("obj1");
+boton1.addEventListener("click",function(){
+    alert("hola soy el boton de restar...");
+});
+
+let boton2 = document.getElementById("obj2");
+boton2.addEventListener("click",function(){
+    alert("holis....");
+});
+
+let boton3 = document.getElementById("obj3");
+boton3.addEventListener("click",function(){
+    alert("hola soy el boton de sumar...");
+});
+
+let numeroelementos = document.getElementById("numeroelementos").innerHTML;
+
+boton3.addEventListener("click",function(){
+    let nodeToAdd = document.createElement("p");
+
+    let nodetext = document.createTextNode("HOLA SOY UN PARRAFO");
+
+    nodeToAdd.appendChild(nodetext);
+
+    cant.append(nodetext);
+    alert(numeroelementos);
+});
