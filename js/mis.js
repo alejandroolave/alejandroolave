1b7e0763531dec657f5087ee5a101a50c8390ac1
MariaTorres7 <maria1033097029@gmail.com>
2023-09-07 10:38:46 -0500

    fe

--- js/mis.js
+++ js/mis.js
@@ -1,6 +1,7 @@
 window.onload = function() {
     var div = document.createElement("div");
     div.innerHTML = "¡Estas son mis habilidades.! ";
+    div.style.fontFamily = "'Segoe UI', Tahoma, Geneva, Verdana, sans-serif"
     div.classList.add("alert");
     document.body.appendChild(div);
     
