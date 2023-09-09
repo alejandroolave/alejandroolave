6ea7773d1c0b9ed500637bbbe470e236504e4075
alejandroolave <olavealejito@gmail.com>
2023-09-09 13:20:12 -0500

    nuevo

--- js/perfil.js
+++ js/perfil.js
@@ -2,3 +2,13 @@ function toggleLinks() {
     var links = document.querySelector(".links");
     links.classList.toggle("hidden");
 }
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+        document.querySelector(".loader").style.display = "none";
+        document.querySelector(".content").style.display = "block"; 
+    }, 4000); 
+});
