0b21a4281a138215de77c96bae1a0314335f8d6b
alejandroolave <olavealejito@gmail.com>
2024-05-30 17:05:15 -0500

    ultimo

--- js/index.js
+++ js/index.js
@@ -101,7 +101,7 @@ function fijarProyecto(id) {
     var srcMap = {
         'proyecto1': 'movimientos.html',
         'proyecto2': 'indexx.html',
-        'proyecto3': 'carta.html',
+        'proyecto3': '',
         'proyecto4': 'https://enlace/proyecto4',
         'proyecto5': 'https://enlace/proyecto5'
     };
